61a8e3cb0b9788fe5045b7a459ab0032773408b9
zipinel <zipinel@yahoo.com>
2023-03-09 23:14:05 +0200

    selfRole command added

---
+++ commands/selfRole.js
@@ -0,0 +1,21 @@
+const { SlashCommandBuilder } = require('discord.js');
+
+module.exports = {
+    data : new SlashCommandBuilder()
+	.setName('selfrole')
+	.setDescription('Setup the roles/tech stack you think fits well with your knowledge')
+	.addStringOption(option =>
+		option.setName('options')
+			.setDescription('options available to choose')
+			.setRequired(true)
+			.addChoices(
+				{ name: 'Python', value: 'Python' },
+				{ name: 'JavaScript', value: 'JavaScript' },
+				{ name: 'C#', value: 'C#' },
+			)),
+    async execute(interaction) {
+    	const option = interaction.options.getString('options');
+    	await interaction.add(option, 'Role added')
+		await interaction.reply(`You have selected ${option}`)
+            },
+}
