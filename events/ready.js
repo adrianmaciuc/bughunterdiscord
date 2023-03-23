98a3d9d56643d2fe18afd434239902eb456b0370
zipinel <zipinel@yahoo.com>
2023-03-23 23:34:03 +0200

    comment out to put whats on dev

--- events/ready.js
+++ events/ready.js
@@ -1,11 +1,11 @@
 const { Events } = require('discord.js');
-const { currentEvents } = require('../database/connection')
+// const { currentEvents } = require('../database/connection')
 
 module.exports = {
 	name: Events.ClientReady,
 	once: true,
 	execute(client) {
-		currentEvents.sync();
+		// currentEvents.sync();
 		console.log(`Logged in as ${client.user.tag}!`);
 	},
 };
