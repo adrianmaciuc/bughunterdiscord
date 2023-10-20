dffc5401371a8876d34722e6090148194334dce0
adrianmaciuc <adrian.maciuc@gmail.com>
2023-10-20 23:19:21 +0300

    role reference

--- channelMsgCommands/sendEmbedMessage.js
+++ channelMsgCommands/sendEmbedMessage.js
@@ -1,9 +1,11 @@
+const { constants : { ROLES } } = require('../support/constants.js');
+
 module.exports = {
 	name: 'sendembed',
 	usage: `!sendembed * Title * Description`,
 	execute(message) {  
 		if (message.author.bot === false 
-			&& message.member.roles.cache.some(role => role.id === '1080571304404594759')
+			&& message.member.roles.cache.some(role => role.id === ROLES.staff)
 			&& message.content.startsWith(process.env.prefix + 'sendembed')) {
 			const extractedArgs = message.content.slice(process.env.prefix.length).trim().split("*");
 
